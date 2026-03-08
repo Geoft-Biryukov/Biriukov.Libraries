92009393269294f260948d63fac5af5737e4ddff
biryukovvg <biryukovvg@gmail.com>
2026-03-08 23:38:24 +0400

    Добавлен проект Mathematics и unit-tests к нему

---
+++ src/Biriukov.Mathematics.Tests/Vector3DTests.cs
@@ -0,0 +1,221 @@
+namespace Biriukov.Mathematics.Tests
+{
+    public class Vector3DTests
+    {
+        private const double Tolerance = 1e-10;
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CanCreateVector3dWithoutParameters()
+        {
+            var v = new Vector3D();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(v.X, Is.EqualTo(0.0).Within(Tolerance));
+                Assert.That(v.Y, Is.EqualTo(0.0).Within(Tolerance));
+                Assert.That(v.Z, Is.EqualTo(0.0).Within(Tolerance));
+            });            
+        }
+
+        [Test]
+        [TestCase(1.0, 2.0, 3.0)]
+        [TestCase(154.0203, 22.125467, 534.00001)]
+        [TestCase(double.NaN, double.NaN, double.NaN)]
+        public void CanCreateVector3dWithParameters(double x, double y, double z)
+        {
+            var v = new Vector3D(x ,y, z);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(v.X, Is.EqualTo(x).Within(Tolerance));
+                Assert.That(v.Y, Is.EqualTo(y).Within(Tolerance));
+                Assert.That(v.Z, Is.EqualTo(z).Within(Tolerance));
+            });
+        }
+
+        #region Test operators
+        [Test]
+        [TestCase(2, 3, 4, 6, 7, 8, 8, 10, 12)]
+        [TestCase( -2, -3, -4, 2, 3, 4, 0, 0, 0)]
+        public void AdditionOperator_ValidVectors_ReturnsCorrectResult(
+        double x1, double y1, double z1,
+        double x2, double y2, double z2,
+        double expectedX, double expectedY, double expectedZ)
+        {
+            // Arrange
+            var v1 = new Vector3D(x1, y1, z1);
+            var v2 = new Vector3D(x2, y2, z2);
+
+            // Act
+            var result = v1 + v2;
+
+            // Assert
+            Assert.Multiple(() =>
+            {                
+                Assert.That(result.X, Is.EqualTo(expectedX).Within(Tolerance));
+                Assert.That(result.Y, Is.EqualTo(expectedY).Within(Tolerance));
+                Assert.That(result.Z, Is.EqualTo(expectedZ).Within(Tolerance));
+            });
+        }
+
+        [Test]
+        [TestCase(6, 7, 8, 2, 3, 4, 4, 4, 4)]
+        [TestCase(0, 0, 0, 2, 3, 4, -2, -3, -4)]
+        public void SubtractionOperator_ValidVectors_ReturnsCorrectResult(
+       double x1, double y1, double z1,
+       double x2, double y2, double z2,
+       double expectedX, double expectedY, double expectedZ)
+        {
+            // Arrange
+            var v1 = new Vector3D(x1, y1, z1);
+            var v2 = new Vector3D(x2, y2, z2);
+
+            // Act
+            var result = v1 - v2;
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.X, Is.EqualTo(expectedX).Within(Tolerance));
+                Assert.That(result.Y, Is.EqualTo(expectedY).Within(Tolerance));
+                Assert.That(result.Z, Is.EqualTo(expectedZ).Within(Tolerance));
+            });
+        }
+        #endregion
+
+        #region Test products
+        /*
+         import numpy as np
+         
+         # Define two 3D vectors
+         vector_a = np.array([1, 2, 3])
+         vector_b = np.array([4, 5, 6])
+         
+         # Calculate the cross product
+         cross_product = np.cross(vector_a, vector_b)
+         
+         # Print the result
+         print(f"Vector A: {vector_a}")
+         print(f"Vector B: {vector_b}")
+         print(f"Cross Product: {cross_product}")
+         */
+        [Test]
+        [TestCase(1, 2, 3, 4, 5, 6, -3,  6, - 3)]
+        public void CrossProduct_ValidVectors_ReturnsCorrectResult(
+            double x1, double y1, double z1,
+            double x2, double y2, double z2,
+            double expectedX, double expectedY, double expectedZ)
+        {
+            // Arrange
+            var v1 = new Vector3D(x1, y1, z1);
+            var v2 = new Vector3D(x2, y2, z2);
+
+            // Act
+            var result = Vector3D.CrossProduct(v1, v2);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.X, Is.EqualTo(expectedX).Within(Tolerance));
+                Assert.That(result.Y, Is.EqualTo(expectedY).Within(Tolerance));
+                Assert.That(result.Z, Is.EqualTo(expectedZ).Within(Tolerance));
+            });
+        }
+
+        [Test]
+        [TestCase(2, 3, 4, 1, 5, 2, 25)]
+        public void DotProduct_ValidVectors_ReturnsCorrectResult(
+            double x1, double y1, double z1,
+            double x2, double y2, double z2,
+            double expected)
+        {
+            // Arrange
+            var v1 = new Vector3D(x1, y1, z1);
+            var v2 = new Vector3D(x2, y2, z2);
+
+            // Act
+            var result = Vector3D.DotProduct(v1, v2);
+
+            // Assert
+            
+            Assert.That(result, Is.EqualTo(expected).Within(Tolerance));                            
+        }
+        #endregion
+
+        #region Test equals
+        [Test]
+        public void Equals_IdenticalVectors_ReturnsTrue()
+        {
+            // Arrange
+            var v1 = new Vector3D(1, 2, 3);
+            var v2 = new Vector3D(1, 2, 3);
+
+            // Act & Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(v1.Equals(v2), Is.True);
+                Assert.That(v1 == v2, Is.True);
+                Assert.That(v1 != v2, Is.False);
+            });
+        }
+
+        [Test]
+        public void Equals_DifferentQuaternions_ReturnsFalse()
+        {
+            // Arrange
+            var v1 = new Vector3D(1, 2, 3);
+            var v2 = new Vector3D(5, 6, 7);
+
+            // Act & Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(v1.Equals(v2), Is.False);
+                Assert.That(v1 == v2, Is.False);
+                Assert.That(v1 != v2, Is.True);
+            });
+        }
+
+        [Test]
+        public void EqualsWithTolerance_NearlyEqualQuaternions_ReturnsTrue()
+        {
+            // Arrange
+            var v1 = new Vector3D(1.0, 2.0, 3.0);
+            var v2 = new Vector3D(1.0000000001, 2.0000000001, 3.0000000001);
+
+            // Act & Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(v1.Equals(v2, 1e-9), Is.True);
+                Assert.That(v1.Equals(v2), Is.False); // Exact comparison should fail
+            });
+        }
+
+        [Test]
+        public void Equals_NullObject_ReturnsFalse()
+        {
+            // Arrange
+            var v = new Vector3D(1, 2, 3);
+            object nullObj = null;
+
+            // Act & Assert
+            Assert.That(v.Equals(nullObj), Is.False);
+        }
+
+        [Test]
+        public void Equals_DifferentType_ReturnsFalse()
+        {
+            // Arrange
+            var v = new Vector3D(1, 2, 3);
+            var notQuaternion = new object();
+
+            // Act & Assert
+            Assert.That(v.Equals(notQuaternion), Is.False);
+        }
+        #endregion
+    }
+}
---
+++ src/Biriukov.Mathematics/Vector3D.cs
@@ -0,0 +1,154 @@
+﻿namespace Biriukov.Mathematics
+{
+    /// <summary>
+    /// Вектор в 3-х мерном пространстве
+    /// </summary>
+    public struct Vector3D : IEquatable<Vector3D>, IFormattable
+    {        
+        /// <summary>
+        /// Создает вектор с компонентами x, y, z
+        /// </summary>
+        /// <param name="x">Компонента x</param>
+        /// <param name="y">Компонента y</param>
+        /// <param name="z">Компонента z</param>
+        public Vector3D(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        /// <summary>
+        /// Компонента x
+        /// </summary>
+        public double X { get; }
+
+        /// <summary>
+        /// Компонента y
+        /// </summary>
+        public double Y { get; }
+
+        /// <summary>
+        /// Компонента z
+        /// </summary>
+        public double Z { get; }
+
+        #region Operators
+
+        /// <summary>
+        /// Унарный плюс
+        /// </summary>
+        /// <param name="v">Вектор, к которому применяется операция</param>
+        /// <returns>Копия исходного вектора</returns>
+        public static Vector3D operator +(Vector3D v) => new(v.X, v.Y, v.Z);
+
+        /// <summary>
+        /// Унарный минус
+        /// </summary>
+        /// <param name="v">Вектор, к которому применяется операция</param>
+        /// <returns>Вектор, противоположный исходному</returns>
+        public static Vector3D operator -(Vector3D v) => new(-v.X, -v.Y, -v.Z);
+
+        /// <summary>
+        /// Сумма двух векторов
+        /// </summary>
+        /// <param name="v1">Первое слагаемое</param>
+        /// <param name="v2">Второе слагаемое</param>
+        /// <returns>Результат сложения</returns>
+        public static Vector3D operator+(Vector3D v1, Vector3D v2)
+            => new Vector3D(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+
+        /// <summary>
+        /// Разность двух векторов
+        /// </summary>
+        /// <param name="v1">Уменьшаемое</param>
+        /// <param name="v2">Вычитаемое</param>
+        /// <returns>Результат вычитания (разность)</returns>
+        public static Vector3D operator -(Vector3D v1, Vector3D v2)
+            => new Vector3D(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+
+        /// <summary>
+        /// Умножение вектора на число
+        /// </summary>
+        /// <param name="d">Скалярный сомножитель</param>
+        /// <param name="v">Векторный сомножитель</param>
+        /// <returns>Результат умножения (произведение)</returns>
+        public static Vector3D operator *(double d, Vector3D v)
+           => new(d * v.X, d * v.Y, d * v.Z);
+
+        /// <summary>
+        /// Умножение вектора на число
+        /// </summary>        
+        /// <param name="v">Векторный сомножитель</param>
+        /// <param name="d">Скалярный сомножитель</param>
+        /// <returns>Результат умножения (произведение)</returns>
+        public static Vector3D operator *(Vector3D v, double d)
+           => new(d * v.X, d * v.Y, d * v.Z);
+
+        /// <summary>
+        /// Деление ветора на число
+        /// </summary>
+        /// <param name="v">Делимое (вектор)</param>
+        /// <param name="d">Делитель (скаляр)</param>
+        /// <returns>Результат деления (вектор)</returns>
+        public static Vector3D operator /(Vector3D v, double d)
+        {
+            double m = 1.0 / d;
+            return new Vector3D(v.X * m, v.Y * m, v.Z * m);
+        }
+
+        public static bool operator ==(Vector3D v1, Vector3D v2) => v1.Equals(v2);        
+
+        public static bool operator !=(Vector3D v1, Vector3D v2) => !v1.Equals(v2);
+        #endregion
+
+        #region Multiplication
+
+        /// <summary>
+        /// Скалярное произведение двух векторов
+        /// </summary>
+        /// <param name="v1">Первый сомножитель</param>
+        /// <param name="v2">Второй сомножитель</param>
+        /// <returns>Результат умножения (скаляр)</returns>
+        public static double DotProduct(Vector3D v1, Vector3D v2)
+            => v1.X * v2.X + v1.Y * v2.Y + v1.Z * v2.Z;
+
+        public double DotProduct(Vector3D v)
+            => X * v.X + Y * v.Y + Z * v.Z;
+
+        /// <summary>
+        /// Векторное произведение двух векторов
+        /// </summary>
+        /// <param name="v1">Левый сомножитель</param>
+        /// <param name="v2">Правый сомножитель</param>
+        /// <returns>Результат умножения (вектор)</returns>
+        public static Vector3D CrossProduct(Vector3D v1, Vector3D v2)
+        {
+            double x = v1.Y * v2.Z - v1.Z * v2.Y;
+            double y = v1.Z * v2.X - v1.X * v2.Z;
+            double z = v1.X * v2.Y - v1.Y * v2.X;
+
+            return new Vector3D(x, y, z);
+        }
+        #endregion
+
+        public bool Equals(Vector3D other, double tolerance = 1e-10)
+            => Math.Abs(X - other.X) < tolerance &&
+             Math.Abs(Y - other.Y) < tolerance &&
+             Math.Abs(Z - other.Z) < tolerance;
+
+        public bool Equals(Vector3D other)
+            => X == other.X && Y == other.Y && Z == other.Z;
+
+        public string ToString(string? format, IFormatProvider? formatProvider)
+        {
+            if (string.IsNullOrEmpty(format))
+                format = @"({0}, {1}, {2})";
+            return string.Format(format, X.ToString(formatProvider), Y.ToString(formatProvider), Z.ToString(formatProvider));
+        }
+
+        public override bool Equals(object obj) => obj is Vector3D other && Equals(other);
+        public override int GetHashCode() => HashCode.Combine(X, Y, Z);
+        public override string ToString() => $"({X:G6}, {Y:G6}, {Z:G6})";
+    }
+}
